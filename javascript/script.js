52f4ac9e528996be5f16febc108f4bb02cd89141
Ryan Pierson <ryanpierson87@protonmail.com>
2018-07-21 16:47:42 -0400

    moved jQuery resize to run first. Removed console.log commands

--- javascript/script.js
+++ javascript/script.js
@@ -6,8 +6,8 @@ var name_check = false;
 var email_check = false;
 var food_check = false;
 $('#other').hide();
+sizeCheck();
 formCheck();
-console.log("Site Load");
 
 //Reveals a text input when "Other is selected
 $('#drop').change(function(){
@@ -33,7 +33,6 @@ $('#name').change(function(){
     name_check = true;
     formCheck();
   } else if(field.length === 0){
-    console.log(nameHeader)
     $('#nameHeader').removeClass('success');
     name_check = false;
     formCheck();
@@ -45,7 +44,6 @@ $('#email').change(function(){
   field = $(this).val();
   console.log(field.length);
   if(field.length < 1){
-    console.log("Return to normal");
     $("#emailHeader").html(same);
     email_check = false;
     formCheck();}else if ((field.indexOf("@") === -1) || !(field.indexOf(".") > field.indexOf("@")) ){
@@ -53,7 +51,6 @@ $('#email').change(function(){
     email_check = false;
     formCheck();
   }else{
-    console.log("emailChanged");
     $("#emailHeader").html('<label class=" px=1" for="email" id="emailHeader">Email</label>');
     $('#emailHeader').addClass('success');
     email_check = true;
@@ -64,23 +61,22 @@ $('#email').change(function(){
 
   //Other Text Input
 function optionCheck(){
-$('#other_input').change(function(){
-  onions = $(this).val().toUpperCase()
-  console.log(onions)
-  if(onions.length === 0){
+  $('#other_input').change(function(){
+    onions = $(this).val().toUpperCase()
+    if(onions.length === 0){
 
-  }else if (onions.indexOf("ONION") === -1){
-    $('#otherHeader').html('<p class="error px=1">Please select something from the list</p>');
-  } else if (onions.indexOf("ONION") >= 0) {
-      $('#otherHeader').html('<p class="success px=1">We haven\'t heard of that. It sounds delicious!</p>');
-      $('#foodHeader').addClass('success');
+    }else if (onions.indexOf("ONION") === -1){
+      $('#otherHeader').html('<p class="error px=1">Please select something from the list</p>');
+    } else if (onions.indexOf("ONION") >= 0) {
+        $('#otherHeader').html('<p class="success px=1">We haven\'t heard of that. It sounds delicious!</p>');
+        $('#foodHeader').addClass('success');
 
-    food_check = true;
-    formCheck();
-  } else{
-    $('#otherHeader').html(other);
-  }
-})
+      food_check = true;
+      formCheck();
+    } else{
+      $('#otherHeader').html(other);
+    }
+  })
 }
   //Name validation
 $().change(function()
@@ -88,67 +84,26 @@ $().change(function()
 
 //submit validation
 function formCheck(){
-  console.log("formCheck ran")
   if(name_check === true && email_check === true && food_check === true){
     $('#submit').removeClass('disabled').
     prop('disabled', false);
   } else{
     $('#submit').addClass('disabled')
       .prop('disabled', true);
-      console.log("ran");
   }
 }
 
 //End of validations
 
 //Resize Reconfiguring
+function sizeCheck(){
 $( window ).resize(function() {
-if($(window).width() < 481)
-    {$("#subhead").hide();
-      $("#head").html(  '<h1 class="display-2 text-white bold" id="head">Hey! Potlucks! Alright!</h1>')
-    } else {
-      $("#subhead").show();
-      $("#head").html(headClone);
-    }
-});
-
-/*=================
-COOKIE FUNCTIONS
-==================*/
-//
-// function getCookie(cname) {
-//   var name = cname + "=";
-//   var decodedCookie = decodeURIComponent(document.cookie);
-//   var ca = decodedCookie.split(';');
-//   for(var i = 0; i <ca.length; i++) {
-//       var c = ca[i];
-//       while (c.charAt(0) == ' ') {
-//           c = c.substring(1);
-//       }
-//       if (c.indexOf(name) == 0) {
-//           return c.substring(name.length, c.length);
-//       }
-//   }
-//   return "";
-// }
-//
-// function VisitCounter(){
-//
-//   var visits = GetCookie("counter");
-//
-//   if (!visits) { visits = 1;
-//
-//     console.log("By the way, this is your first time here.");
-//
-//   } else {
-//
-//     visits = parseInt(visits) + 1;
-//
-//     console.log("I note, you have been here " + visits + " times.");}
-//
-//     setCookie("counter", visits);
-//   }
-//
-//
-//
-//   VisitCounter();
+  if($(window).width() < 481)
+      {$("#subhead").hide();
+        $("#head").html(  '<h1 class="display-2 text-white bold" id="head">Hey! Potlucks! Alright!</h1>')
+      } else {
+        $("#subhead").show();
+        $("#head").html(headClone);
+      }
+  });
+}
